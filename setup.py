ee3bb033cd19906ff4864877fca0983f58c27420
miokato <miokato@me.com>
2018-03-13 22:59:17 +0900

    Add dependency_links to setup

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     long_description='readme',
     author='Mio Kato',
     author_email='test@example.com',
-    packages = find_packages(),
+    packages=find_packages(),
+    install_requires=['requests'],
+    dependency_links=['git+ssh://git@github.com/miokato/sample.git#egg=sample'],
     test_suite='tests',
 )
