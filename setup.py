34efbc40220e6870f4a402dfdfa37a926e30ea5d
miokato <miokato@me.com>
2018-03-13 23:01:46 +0900

    Fix adding sample to install_requires on setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Mio Kato',
     author_email='test@example.com',
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=['requests', 'sample'],
     dependency_links=['git+ssh://git@github.com/miokato/sample.git#egg=sample'],
     test_suite='tests',
 )
